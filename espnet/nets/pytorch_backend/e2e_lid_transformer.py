46f93d02040c8636f685de74ebfcfbcdf291c190
luyizhou4 <luyizhou4@gmail.com>
2019-12-23 16:51:08 +0800

    frame level lid classifier

---
+++ espnet/nets/pytorch_backend/e2e_lid_transformer.py
@@ -0,0 +1,214 @@
+# Copyright 2019 Shigeki Karita
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+"""Transformer speech recognition model (pytorch)."""
+
+from argparse import Namespace
+from distutils.util import strtobool
+
+import logging
+import math
+
+import torch
+import chainer
+from chainer import reporter
+
+from espnet.nets.asr_interface import ASRInterface
+from espnet.nets.pytorch_backend.ctc import CTC
+from espnet.nets.pytorch_backend.e2e_asr import CTC_LOSS_THRESHOLD
+from espnet.nets.pytorch_backend.nets_utils import make_pad_mask
+from espnet.nets.pytorch_backend.nets_utils import th_accuracy
+from espnet.nets.pytorch_backend.transformer.add_sos_eos import add_sos_eos
+from espnet.nets.pytorch_backend.transformer.attention import MultiHeadedAttention
+from espnet.nets.pytorch_backend.transformer.decoder import Decoder
+from espnet.nets.pytorch_backend.transformer.encoder import Encoder
+from espnet.nets.pytorch_backend.transformer.initializer import initialize
+from espnet.nets.pytorch_backend.transformer.label_smoothing_loss import LabelSmoothingLoss, LanguageIDMultitakLoss
+from espnet.nets.pytorch_backend.transformer.mask import subsequent_mask
+from espnet.nets.pytorch_backend.transformer.mask import target_mask
+from espnet.nets.pytorch_backend.transformer.plot import PlotAttentionReport
+from espnet.nets.scorers.ctc import CTCPrefixScorer
+
+class Reporter(chainer.Chain):
+    """A chainer reporter wrapper."""
+
+    def report(self, acc, loss):
+        """Report at every step."""
+        reporter.report({'acc': acc}, self)
+        reporter.report({'loss': loss}, self)
+
+class E2E(ASRInterface, torch.nn.Module):
+    """E2E module.
+
+    :param int idim: dimension of inputs
+    :param int odim: dimension of outputs
+    :param Namespace args: argument Namespace containing options
+
+    """
+
+    @staticmethod
+    def add_arguments(parser):
+        """Add arguments."""
+        group = parser.add_argument_group("transformer model setting")
+
+        group.add_argument("--transformer-init", type=str, default="pytorch",
+                           choices=["pytorch", "xavier_uniform", "xavier_normal",
+                                    "kaiming_uniform", "kaiming_normal"],
+                           help='how to initialize transformer parameters')
+        group.add_argument("--transformer-input-layer", type=str, default="conv2d",
+                           choices=["conv2d", "linear", "embed"],
+                           help='transformer input layer type')
+        group.add_argument('--transformer-attn-dropout-rate', default=None, type=float,
+                           help='dropout in transformer attention. use --dropout-rate if None is set')
+        group.add_argument('--transformer-lr', default=10.0, type=float,
+                           help='Initial value of learning rate')
+        group.add_argument('--transformer-warmup-steps', default=25000, type=int,
+                           help='optimizer warmup steps')
+        group.add_argument('--transformer-length-normalized-loss', default=True, type=strtobool,
+                           help='normalize loss by length')
+
+        group.add_argument('--dropout-rate', default=0.0, type=float,
+                           help='Dropout rate for the encoder')
+        # Encoder
+        group.add_argument('--elayers', default=4, type=int,
+                           help='Number of encoder layers (for shared recognition part in multi-speaker asr mode)')
+        group.add_argument('--eunits', '-u', default=300, type=int,
+                           help='Number of encoder hidden units')
+        # Attention
+        group.add_argument('--adim', default=320, type=int,
+                           help='Number of attention transformation dimensions')
+        group.add_argument('--aheads', default=4, type=int,
+                           help='Number of heads for multi head attention')
+        # Decoder
+        group.add_argument('--dlayers', default=1, type=int,
+                           help='Number of decoder layers')
+        group.add_argument('--dunits', default=320, type=int,
+                           help='Number of decoder hidden units')
+        return parser
+
+    @property
+    def attention_plot_class(self):
+        """Return PlotAttentionReport."""
+        return PlotAttentionReport
+
+    def __init__(self, idim, odim, args, ignore_id=-1):
+        """Construct an E2E object.
+
+        :param int idim: dimension of inputs
+        :param int odim: dimension of outputs
+        :param Namespace args: argument Namespace containing options
+        """
+        torch.nn.Module.__init__(self)
+        if args.transformer_attn_dropout_rate is None:
+            args.transformer_attn_dropout_rate = args.dropout_rate
+        self.encoder = Encoder(
+            idim=idim,
+            attention_dim=args.adim,
+            attention_heads=args.aheads,
+            linear_units=args.eunits,
+            num_blocks=args.elayers,
+            input_layer=args.transformer_input_layer,
+            dropout_rate=args.dropout_rate,
+            positional_dropout_rate=args.dropout_rate,
+            attention_dropout_rate=args.transformer_attn_dropout_rate
+        )
+        self.odim = odim
+        self.ignore_id = ignore_id
+        self.subsample = [1]
+        self.reporter = Reporter()
+
+        # self.lsm_weight = a
+        self.criterion = LabelSmoothingLoss(self.odim, self.ignore_id, args.lsm_weight,
+                                            args.transformer_length_normalized_loss)
+        self.reset_parameters(args)
+        self.lid_lo = torch.nn.Linear(args.adim, self.odim)
+        # yzl23 config
+        logging.warning(self)
+
+    def reset_parameters(self, args):
+        """Initialize parameters."""
+        # initialize parameters
+        initialize(self, args.transformer_init)
+
+    def forward(self, xs_pad, ilens, ys_pad):
+        """E2E forward.
+
+        :param torch.Tensor xs_pad: batch of padded source sequences (B, Tmax, idim)
+        :param torch.Tensor ilens: batch of lengths of source sequences (B)
+        :param torch.Tensor ys_pad: batch of padded target sequences (B, Lmax)
+        :return: ctc loass value
+        :rtype: torch.Tensor
+        :return: attention loss value
+        :rtype: torch.Tensor
+        :return: accuracy in attention decoder
+        :rtype: float
+        """
+        # forward encoder
+        xs_pad = xs_pad[:, :max(ilens)]  # for data parallel
+        src_mask = (~make_pad_mask(ilens.tolist())).to(xs_pad.device).unsqueeze(-2)
+        hs_pad, hs_mask = self.encoder(xs_pad, src_mask)
+        # lid output layer
+        pred_pad = self.lid_lo(hs_pad)
+
+        # compute lid loss
+        self.loss = self.criterion(pred_pad, ys_pad)
+        self.acc = th_accuracy(pred_pad.view(-1, self.odim), ys_pad,
+                               ignore_label=self.ignore_id)
+
+        loss_data = float(self.loss)
+        if loss_data < CTC_LOSS_THRESHOLD and not math.isnan(loss_data):
+            self.reporter.report(self.acc, loss_data)
+        else:
+            logging.warning('loss (=%f) is not correct', loss_data)
+        return self.loss
+
+    def encode(self, x):
+        """Encode acoustic features.
+
+        :param ndarray x: source acoustic feature (T, D)
+        :return: encoder outputs
+        :rtype: torch.Tensor
+        """
+        self.eval()
+        x = torch.as_tensor(x).unsqueeze(0) # (B, T, D) with #B=1
+        enc_output, _ = self.encoder(x, None)
+        return enc_output.squeeze(0) # returns tensor(T, D)
+
+    def recognize(self, x, recog_args, char_list=None, rnnlm=None, use_jit=False):
+        """Recognize input speech with ctc greedy decoding.
+
+        :param ndnarray x: input acoustic feature (B, T, D) or (T, D)
+        :param Namespace recog_args: argment Namespace contraining options
+        :return: N-best decoding results (fake results for compatibility)
+        :rtype: list
+        """
+        enc_output = self.encode(x).unsqueeze(0) # (1, T, D)
+        lpz = self.lid_lo(enc_output)
+        lpz = lpz.squeeze(0) # shape of (T, D)
+        idx = lpz.argmax(-1).cpu().numpy().tolist()
+        hyp = {}
+        # [-1] is added here to be compatible with S2S decoding, 
+        # file: espnet/asr/asr_utils/parse_hypothesis
+        hyp['yseq'] = [-1] + idx # not apply ctc mapping, to get ctc alignment
+        logging.info(hyp['yseq'])
+        hyp['score'] = -1
+        return [hyp]
+
+    def calculate_all_attentions(self, xs_pad, ilens, ys_pad):
+        """E2E attention calculation.
+
+        :param torch.Tensor xs_pad: batch of padded input sequences (B, Tmax, idim)
+        :param torch.Tensor ilens: batch of lengths of input sequences (B)
+        :param torch.Tensor ys_pad: batch of padded token id sequence tensor (B, Lmax)
+        :return: attention weights with the following shape,
+            1) multi-head case => attention weights (B, H, Lmax, Tmax),
+            2) other case => attention weights (B, Lmax, Tmax).
+        :rtype: float ndarray
+        """
+        with torch.no_grad():
+            self.forward(xs_pad, ilens, ys_pad)
+        ret = dict()
+        for name, m in self.named_modules():
+            if isinstance(m, MultiHeadedAttention):
+                ret[name] = m.attn.cpu().numpy()
+        return ret
